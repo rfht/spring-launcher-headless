6e43ad7cd617642f47d987418c7571bb9189d389
Marek Rusinowski <marekrusinowski@gmail.com>
2023-04-17 11:42:02 +0200

    Update pr-downloader binaries

--- src/main.js
+++ src/main.js
@@ -16,14 +16,14 @@ if (!isFirstInstance) {
 // https://github.com/beyond-all-reason/pr-downloader/issues/48
 // Once it's resolved, the commit that added this piece of code
 // can be fully reverted.
-if (process.platform == 'win32' && !('SSL_CERT_FILE' in process.env)) {
+if (process.platform == 'win32' && !('PRD_SSL_CERT_FILE' in process.env)) {
 	const path = require('path');
 	const fs = require('fs');
 	let cacertPath = path.resolve(`${__dirname}/../bin/cacert.pem`);
 	if (!fs.existsSync(cacertPath)) {
 		cacertPath = path.resolve(`${process.resourcesPath}/../bin/cacert.pem`);
 	}
-	process.env['SSL_CERT_FILE'] = cacertPath;
+	process.env['PRD_SSL_CERT_FILE'] = cacertPath;
 }
 
 const { log } = require('./spring_log');
