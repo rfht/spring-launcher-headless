3bda51cfec360ebbd2fe3976f473c2bc1983d03b
Marek Rusinowski <marekrusinowski@gmail.com>
2024-01-04 00:11:51 +0100

    Add another hack for the window sizing on Windows
    There are very small differences between title bar heights that cause small
    flicker or little bit of scrolling possible. This change is a combination of
    CSS fix to eliminate scrolling + JS haight caching to remove the main launcher
    part resizing when opening and closing low window.

--- src/renderer/renderer_misc.js
+++ src/renderer/renderer_misc.js
@@ -47,4 +47,19 @@ for (const btn of document.querySelectorAll('button')) {
 	root.style.setProperty('--window-height', `${dimensions.windowHeight}px`);
 	root.style.setProperty('--window-width', `${dimensions.windowWidth}px`);
 	root.style.setProperty('--infobox-height', `${dimensions.infoboxHeight}px`);
+
+	// This is a hack to reduce flicker because of inacuraccies with the window
+	// sizing and content sizing on Windows.
+	//
+	// We use the 100hw reference in styles to make sure that content is never
+	// scrollable, but there is still some resizing in some cases when togling
+	// the log window. This hack effectively permamently caches the *real* hight
+	// of the window so that when we open  the log window, there isn't any
+	// flicker present. The 100ms delay is entirely arbitrary for when window
+	// "stabilizes" enough to be able to take the measurement.
+	setTimeout(() => {
+		const mainContent = document.getElementById('main-content');
+		const computedHeight = getComputedStyle(mainContent).getPropertyValue('height');
+		root.style.setProperty('--window-height', computedHeight);
+	}, 150);
 }
